496f9149e65836e5d8068387be91729e1b3b8e9d
zirui-song <zrsong@mit.edu>
2024-08-27 00:12:58 -0400

    Download Annual Compustat Data to Generate Sumstat table

--- Download_Data.py
+++ Download_Data.py
@@ -6,7 +6,7 @@ db = wrds.Connection(wrds_username='zrsong')
 
 # Define the start and end dates
 start_date = '2010-01-01'
-end_date = '2023-12-31'
+end_date = '2024-06-30'
 
 '''
 # Use S&P ratings data as Compustat ratings data were discontinued in 2017
@@ -32,7 +32,22 @@ query = f"""
 lendershares = db.raw_sql(query)
 
 # output csv. format
-lendershares.to_csv("Data/Raw/lendershares.csv", index=False)
+lendershares.to_csv("../Data/Raw/lendershares.csv", index=False)
+
+# Annual Compustat data
+fund_table = 'funda'
+
+# use the varlist above to query quarterly compustat data (don't use join(varlist) as it will return a string)
+query = f"""
+    SELECT *
+    FROM compa.{fund_table}
+    WHERE datadate >= '{start_date}' AND datadate <= '{end_date}'
+"""
+
+compa = db.raw_sql(query)
+
+# output csv. format
+compa.to_csv("../Data/Raw/compustat_annual.csv", index=False)
 
 # Quarterly Compustat data
 fund_table = 'fundq'
@@ -52,7 +67,7 @@ query = f"""
 compq = db.raw_sql(query)
 
 # output csv. format
-compq.to_csv("Data/Raw/compustat_quarterly.csv", index=False)
+compq.to_csv("../Data/Raw/compustat_quarterly.csv", index=False)
 
 
 # Query the dealscan syndicated loan data
@@ -66,7 +81,7 @@ query = f"""
 dealscan_data = db.raw_sql(query)
 
 # Save the data to a CSV file
-dealscan_data.to_csv('Data/Raw/dealscan_data.csv', index=False)
+dealscan_data.to_csv('../Data/Raw/dealscan_data.csv', index=False)
 
 # Covenant levels are then merged with accounting data available from Compustat using a link file provided by Michael Roberts and Sudheer Chava (as used in Chava and Roberts (2008))
 # Query the link file
